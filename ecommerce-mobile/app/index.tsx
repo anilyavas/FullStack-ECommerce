61419a9ae907ae0664449c29adf62e280bfee472
anilyavas <anil.yavas99@gmail.com>
2024-10-20 17:09:14 +0300

    setup expo router

---
+++ ecommerce-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Hello Home</Text>
+    </View>
+  );
+}
