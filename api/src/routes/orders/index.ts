32de7cd0322e1ad37a5e7f88a15276f66580edda
anilyavas <anil.yavas99@gmail.com>
2024-10-20 13:20:09 +0300

    Orders and order items create

---
+++ api/src/routes/orders/index.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { createOrder } from './ordersControllers.js';
+import { validateData } from '../../middlewares/validationMiddleware.js';
+import {
+  insertOrderSchema,
+  insertOrderWithItemsSchema,
+} from '../../db/ordersSchema.js';
+import { verifyToken } from '../../middlewares/authMiddleware.js';
+
+const router = Router();
+
+router.post(
+  '/',
+  verifyToken,
+  validateData(insertOrderWithItemsSchema),
+  createOrder
+);
+
+export default router;
