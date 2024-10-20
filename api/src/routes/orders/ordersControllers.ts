01db9b30fc35ca7060ee938215e7114f4e8ad703
anilyavas <anil.yavas99@gmail.com>
2024-10-20 16:51:14 +0300

    startup point of mobile app

--- api/src/routes/orders/ordersControllers.ts
+++ api/src/routes/orders/ordersControllers.ts
@@ -12,6 +12,7 @@ export async function createOrder(req: Request, res: Response) {
     }
     const [newOrder] = await db
       .insert(ordersTable)
+      // @ts-ignore
       .values({ userId: userId })
       .returning();
 
