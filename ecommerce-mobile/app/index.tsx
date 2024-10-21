8fd78eddf6801fb4a9e11da3fcea78b6a9e2c9e0
anilyavas <anil.yavas99@gmail.com>
2024-10-21 14:31:12 +0300

    Fix web and make it responsive

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,15 +1,23 @@
 import { FlatList } from 'react-native';
 import products from '../assets/products.json';
 import ProductListItem from '../components/ProductListItem';
+import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
 
 export default function HomeScreen() {
+  const numColumns = useBreakpointValue({
+    default: 2,
+    sm: 3,
+    xl: 4,
+  });
+
   return (
     <FlatList
-      contentContainerClassName='gap-2'
+      key={numColumns}
+      contentContainerClassName='gap-2 max-w-[960px]'
       columnWrapperClassName='gap-2'
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
-      numColumns={2}
+      numColumns={numColumns}
     />
   );
 }
