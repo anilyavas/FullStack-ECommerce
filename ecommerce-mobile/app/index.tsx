8dee52819f529e0d01c4a26fcc1dcee1b04da590
anilyavas <anil.yavas99@gmail.com>
2024-10-21 11:45:37 +0300

    Product item UI with gluestack

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,16 +1,15 @@
 import { FlatList } from 'react-native';
 import products from '../assets/products.json';
 import ProductListItem from '../components/ProductListItem';
-import { Button, ButtonText } from '@/components/ui/button';
 
 export default function HomeScreen() {
   return (
-    <Button>
-      <ButtonText>Press me</ButtonText>
-    </Button>
-    /*<FlatList
+    <FlatList
+      contentContainerClassName='gap-2'
+      columnWrapperClassName='gap-2'
       data={products}
       renderItem={({ item }) => <ProductListItem item={item} />}
-    />*/
+      numColumns={2}
+    />
   );
 }
--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,4 +1,10 @@
-import { View, Text } from 'react-native';
+import { Card } from '@/components/ui/card';
+import { Image } from '@/components/ui/image';
+import { VStack } from '@/components/ui/vstack';
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import { Box } from '@/components/ui/box';
+import { Button, ButtonText } from '@/components/ui/button';
 
 type Product = {
   id: number;
@@ -10,8 +16,37 @@ type Product = {
 
 export default function ProductListItem({ item }: { item: Product }) {
   return (
-    <View>
-      <Text style={{ fontSize: 30 }}>{item.name}</Text>
-    </View>
+    <Card className='p-5 rounded-lg max-w-[360px] flex-1'>
+      <Image
+        source={{
+          uri: item.image,
+        }}
+        alt={`${item.name} image`}
+        className='mb-6 h-[240px] w-full rounded-md'
+        resizeMode='contain'
+      />
+      <Text className='text-sm font-normal mb-2 text-typography-700'>
+        {item.name}
+      </Text>
+      <VStack className='mb-6'>
+        <Heading size='md' className='mb-4'>
+          ${item.price}
+        </Heading>
+        <Text size='sm'>{item.description}</Text>
+      </VStack>
+      <Box className='flex-col sm:flex-row'>
+        <Button className='px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1'>
+          <ButtonText size='sm'>Add to cart</ButtonText>
+        </Button>
+        <Button
+          variant='outline'
+          className='px-4 py-2 border-outline-300 sm:flex-1'
+        >
+          <ButtonText size='sm' className='text-typography-600'>
+            Wishlist
+          </ButtonText>
+        </Button>
+      </Box>
+    </Card>
   );
 }
