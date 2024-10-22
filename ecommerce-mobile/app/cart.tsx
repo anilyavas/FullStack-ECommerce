5c73d164883257d7eb01647e7defff675174f719
anilyavas <anil.yavas99@gmail.com>
2024-10-22 20:32:26 +0300

    Submit orders

--- ecommerce-mobile/app/cart.tsx
+++ ecommerce-mobile/app/cart.tsx
@@ -7,14 +7,33 @@ import { Text } from '@/components/ui/text';
 import { Image } from '@/components/ui/image';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Redirect } from 'expo-router';
+import { useMutation } from '@tanstack/react-query';
+import { createOrder } from '@/api/orders';
 
 export default function CartScreen() {
   const items = useCart((state) => state.items);
   const resetCart = useCart((state) => state.resetCart);
 
+  const createOrderMutation = useMutation({
+    mutationFn: () =>
+      createOrder(
+        items.map((item) => ({
+          productId: item.product.id,
+          quantity: item.quantity,
+          price: item.product.price, // manage from server side!
+        }))
+      ),
+    onSuccess: (data) => {
+      console.log(data);
+      resetCart();
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   const onCheckout = async () => {
-    //send order to server
-    resetCart();
+    createOrderMutation.mutate();
   };
 
   if (items.length === 0) {
